04cf0ed72ba99d35abf8ebd48e57f6a267acbafa
Suresh Kotla <skotla@delta.org>
2020-07-28 13:29:00 -0700

    add test code

---
+++ HelloWorld/src/main/java/helloworld/helloworld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author ca32585
+ *
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	
+
+	    public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+	
+
+
+	
+
+}
